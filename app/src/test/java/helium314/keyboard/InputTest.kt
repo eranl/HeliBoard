8e5d6c8aaa846d7e56b92f2126fdc81fd1bfd988
Helium314 <helium314@mailbox.org>
2026-07-12 09:47:24 +0200

    expand InputTest
    slidingInputFromCapsLock should fail after merging #2586

--- app/src/test/java/helium314/keyboard/InputTest.kt
+++ app/src/test/java/helium314/keyboard/InputTest.kt
@@ -10,6 +10,7 @@ import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowLog
+import org.robolectric.shadows.ShadowLooper
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -18,6 +19,7 @@ import kotlin.test.assertEquals
 @RunWith(RobolectricTestRunner::class)
 @Config(shadows = [
     ShadowInputMethodService::class,
+    ShadowLooper::class,
 ])
 class InputTest {
     private val latinIME = Robolectric.setupService(LatinIME::class.java)
@@ -31,26 +33,65 @@ class InputTest {
         keyboardSwitcher.reloadMainKeyboard()
     }
 
-    @Test fun down() {
+    @Test fun pressShift() {
         assertEquals(KeyboardElement.ALPHABET, keyboardSwitcher.keyboard?.mId?.element)
         touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN)
         assertEquals(KeyboardElement.ALPHABET_MANUAL_SHIFTED, keyboardSwitcher.keyboard?.mId?.element)
     }
 
-    @Test fun downUp() {
+    @Test fun keyInput() {
         touchKey('a'.code, MotionEvent.ACTION_DOWN)
         touchKey('a'.code, MotionEvent.ACTION_UP)
         assertEquals("a", ShadowInputMethodService.text)
     }
 
-    private fun touchKey(code: Int, action: Int) {
+    @Test fun holdShift() {
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN)
+        assertEquals(KeyboardElement.ALPHABET_MANUAL_SHIFTED, keyboardSwitcher.keyboard?.mId?.element)
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks() // this doesn't actually wait, just results in handling the delayed message immediately
+        assertEquals(KeyboardElement.ALPHABET_SHIFT_LOCKED, keyboardSwitcher.keyboard?.mId?.element)
+    }
+
+    @Test fun slidingInput() {
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN)
+        touchKey('F'.code, MotionEvent.ACTION_MOVE)
+        touchKey('F'.code, MotionEvent.ACTION_UP)
+        assertEquals("F", ShadowInputMethodService.text)
+        assertEquals(KeyboardElement.ALPHABET, keyboardSwitcher.keyboard?.mId?.element)
+    }
+
+    @Test fun endCapsLockWithShift() {
+        // keyboardSwitcher.setAlphabetShiftLockedKeyboard() isn't enough because it doesn't set the fully correct state
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN)
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_UP)
+
+        // need to set event time to prevent mTouchNoiseThresholdTime thing triggering (todo: should be done automatically in the test)
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN, 50L)
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_UP, 50L)
+        assertEquals(KeyboardElement.ALPHABET, keyboardSwitcher.keyboard?.mId?.element)
+    }
+
+    @Test fun slidingInputFromCapsLock() {
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN)
+        ShadowLooper.runUiThreadTasksIncludingDelayedTasks()
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_UP)
+
+        touchKey(KeyCode.SHIFT, MotionEvent.ACTION_DOWN, 50L)
+        touchKey('F'.code, MotionEvent.ACTION_MOVE)
+        touchKey('F'.code, MotionEvent.ACTION_UP)
+        assertEquals("F", ShadowInputMethodService.text)
+        assertEquals(KeyboardElement.ALPHABET_SHIFT_LOCKED, keyboardSwitcher.keyboard?.mId?.element)
+    }
+
+    private fun touchKey(code: Int, action: Int, eventTime: Long = 0L) {
         val kb = keyboardSwitcher.keyboard!!
         val key = kb.getKey(code)!!
         val x = key.x + key.height / 2
         val y = key.y + key.width / 2
         val me = MotionEvent.obtain( // todo: there are many more parameters, also related to pointers
             0L,
-            0L,
+            eventTime,
             action,
             x.toFloat(),
             y.toFloat(),
@@ -61,6 +102,7 @@ class InputTest {
 
     @BeforeTest
     fun reset() {
+        keyboardSwitcher.reloadMainKeyboard()
         ShadowInputMethodService.reset()
     }
 }
